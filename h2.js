92ec1bca07e8e5726428249ec72ce403a0d65f2e
HVM1 <HVM1@HVM1.com>
2020-04-26 17:46:01 -0700

    initial first commit 3
    initial first commit 3

---
+++ h2.js
@@ -0,0 +1,29 @@
+let path = anime.path('path');
+
+var tl = anime.timeline({
+  targets: "path",
+  opacity: 0,
+  duration: 20000,
+  loop: true,
+  direction: "linear",
+  easing: "easeInOutExpo",
+  autoplay: true,
+});
+
+tl.add({
+  targets: ".hdiv",
+  translateX: path("x"),
+  translateY: path("y"),
+  rotate: path("angle"),
+  duration: 10000,
+  loop: true,
+  easing: "linear",
+}).add({
+  targets: ".hdiv",
+  translateX: path("x"),
+  translateY: path("y"),
+  rotate: path("angle"),
+  duration: 20000,
+  loop: true,
+  easing: "linear",
+});
